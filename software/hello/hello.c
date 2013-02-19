4fafbc917990b06063fdac3825a38c48dda80839
Nino Ninux <nino.ninux@gmail.com>
2013-02-19 21:44:26 +0100

    erstes programm kompiliert und geflasht

---
+++ software/hello/hello.c
@@ -0,0 +1,18 @@
+#include  <msp430.h>
+
+int main(void)
+{
+        WDTCTL = WDTPW + WDTHOLD;               // Stop watchdog timer
+        P1DIR = 0x40;               // P1.6 output (green LED)
+        P1OUT = 0;              // LED off
+
+        for (;;)                // Loop forever
+        {
+            volatile unsigned long i;
+            P1OUT ^= 0x40;          // Toggle P1.6 output (green LED) using exclusive-OR
+            i = 99999;                      // Delay
+
+            do (i--);           // busy waiting (bad)
+            while (i != 0);
+        }
+}
