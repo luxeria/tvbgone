8eab10451bc7aa17e8ebacb596073625a130b5ce
Nino Ninux <nino.ninux@gmail.com>
2013-02-19 21:54:43 +0100

    hello.c an code-konventionen angepasst

--- software/hello/hello.c
+++ software/hello/hello.c
@@ -1,18 +1,20 @@
+/* Example-Code from http://wiki.ubuntuusers.de/MSP430-Toolchain */
+
 #include  <msp430.h>
 
 int main(void)
 {
-        WDTCTL = WDTPW + WDTHOLD;               // Stop watchdog timer
-        P1DIR = 0x40;               // P1.6 output (green LED)
-        P1OUT = 0;              // LED off
+        WDTCTL = WDTPW + WDTHOLD;           /* Stop watchdog timer */
+        P1DIR = 0x40;                       /* P1.6 output (green LED) */
+        P1OUT = 0;                          /* LED off */
 
-        for (;;)                // Loop forever
+        for (;;)                            /* Loop forever */
         {
-            volatile unsigned long i;
-            P1OUT ^= 0x40;          // Toggle P1.6 output (green LED) using exclusive-OR
-            i = 99999;                      // Delay
+                volatile unsigned long i;
+                P1OUT ^= 0x40;              /* Toggle P1.6 output with XOR */
+                i = 99999;                  /* Delay */
 
-            do (i--);           // busy waiting (bad)
-            while (i != 0);
+                do (i--);                   /* busy waiting (bad) */
+                while (i != 0);
         }
 }
